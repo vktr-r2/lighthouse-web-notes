c874b0d23b72c0449a8a1f1bc590f62ee817468e
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-07 09:33:54 -0400

    Initial Commit - add objects-properties-keys-values.js practise file, add two example objects

---
+++ Week_1/Day_3/objects-properties-keys-values.js
@@ -0,0 +1,25 @@
+const me = {
+  name: "Viktor",
+  age: 32,
+  height: 185,
+  hair: "brun",
+  glasses: true,
+  hobbies: ["golf" , "movies" , "hiking", "biking", "learning to code"]
+}
+
+const cats = [
+  {
+    name: "Batman",
+    colour: "Black",
+    age: 15,
+    friendly: true,
+    fav_foods: ["butter" , "fries", "tuna juice", "poultry"]
+  },
+  {
+    name: "Luna",
+    colour: "Black",
+    age: 4,
+    friendly: true,
+    fav_foods: ["tuna juice" , "cheetos", "poultry", "kibble"]
+  }
+]
